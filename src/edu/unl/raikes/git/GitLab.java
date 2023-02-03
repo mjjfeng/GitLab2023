b825cb031ed0325e2b0a3d4e63142ea8b43258a9
victoriach231 <victoriach231@gmail.com>
2023-02-03 12:38:35 -0600

    Changed GitLab to use a helper class

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -7,6 +7,6 @@ public class GitLab {
     }
     
     public static void main(String[] args) {
-        System.out.println("This is a lesson on git");
-    }
+        GitLabHelper helper = new GitLabHelper("Victoria", "Josh");
+        helper.PrintWelcomeMessage();    }
 }
